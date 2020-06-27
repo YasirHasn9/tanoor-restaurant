c21142f2fa930a737730ca5b3caa9c47b9a9ae21
YasirHasn9 <hassanyasir223@gmail.com>
2020-06-27 11:39:23 -0700

    styled the images

--- client/src/components/food/FoodImages.js
+++ client/src/components/food/FoodImages.js
@@ -6,9 +6,25 @@ const Ul = styled.ul`
   display: flex;
   justify-content: space-around;
   li {
-    width: 25%;
+    width: 20%;
+    margin: 1%;
+    overflow: hidden;
+    background: #000;
+    border-radius: 20px;
     img {
       width: 100%;
+      height: 100%;
+      -webkit-transform: scale(1.15);
+      transform: scale(1.15);
+      -webkit-transition: all 0.4s;
+      transition: all 0.4s;
+      opacity: 0.6;
+      &:hover {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+        z-index: 2;
+        opacity: 1;
+      }
     }
   }
 `;
